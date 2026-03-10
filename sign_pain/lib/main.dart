cc1548bd3c76b66fdca510db44b62d359e126986
Rpaulo22 <rodrigopaulo2002@gmail.com>
2026-03-10 15:58:19 +0000

    added firebase dependencies

--- sign_pain/lib/main.dart
+++ sign_pain/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:sign_pain/view/home_page_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
