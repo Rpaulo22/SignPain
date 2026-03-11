61b6ee60be1292a2a839b398ebcbca5275415390
Rpaulo22 <rodrigopaulo2002@gmail.com>
2026-03-11 18:33:25 +0000

    updated ui on pain history

--- sign_pain/lib/view/pain_info_screen.dart
+++ sign_pain/lib/view/pain_info_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sign_pain/model/pain_form_data.dart';
 import 'package:sign_pain/viewmodel/form_view_model.dart';
+import 'package:intl/intl.dart';
 
 class PainInfoScreen extends StatefulWidget {
   const PainInfoScreen({super.key});
@@ -32,15 +33,23 @@ class _PainInfoScreenState extends State<PainInfoScreen> {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) { // pain data has not been loaded yet
             return const Center(child: CircularProgressIndicator());
+
           } else if (snapshot.hasError) { // data loading has incurred in some error
             return Center(child: Text("Erro a carregar página: ${snapshot.error}"));
+
           } else if (snapshot.hasData) { // data has been loaded
             final data = snapshot.data!;
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                return ListTile(title: Text("Nível de dor: ${data[index].painLevel} | Data: ${data[index].date}"));
-              },
+            return Column(
+              children: <Widget>[
+                for (var entry in data) 
+                  painFormWidget(entry),
+                  const Divider(
+                    thickness: 5,
+                    indent: 25,
+                    endIndent: 25,
+                    color: Colors.transparent,
+                  )
+              ]
             );
           }
           return const Center(child: Text("Não tem quaisquer registos de dor"));
@@ -48,4 +57,44 @@ class _PainInfoScreenState extends State<PainInfoScreen> {
 		  )
     );
 	}
+
+  // individual pain form entry
+  Widget painFormWidget(PainFormData data) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Theme.of(context).colorScheme.primaryContainer)
+      ),
+      padding: EdgeInsetsDirectional.only(top: 15, bottom: 15, start: 10, end: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Dor registada: ${data.painLevel}/10", textScaler: TextScaler.linear(1.2)),
+              Text("Data: ${DateFormat('dd-MM-yyy | kk:mm').format(data.date!)}", textScaler: TextScaler.linear(1.2), style: TextStyle(fontWeight: FontWeight.bold))
+            ]
+          ),
+          const Divider(
+            thickness: 5,
+            indent: 10,
+            endIndent: 10,
+            color: Colors.transparent,
+          ),
+          RichText(
+            textAlign: TextAlign.start,
+            text: TextSpan(
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+              children: <TextSpan>[
+                TextSpan(text: "Descrição da dor: "),
+                TextSpan(text: data.descriptors.isNotEmpty ? data.descriptors.join(", ") : "Nenhuma", style: const TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            )
+          )
+        ],
+      )
+    );
+  }
 }
